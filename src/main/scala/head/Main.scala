313a0aacc9ec4f041c6aa2f3b8cfea309dde4cd9
Weiqifan1 <cmlykke@hotmail.com>
2021-06-11 20:41:09 +0200

    refactored such that input system lookout is in a separate directory

--- src/main/scala/head/Main.scala
+++ src/main/scala/head/Main.scala
@@ -1,6 +1,7 @@
 package head
 
 import dataClasses.{cedictMaps, codeToTextList, codeToTextObject, frequencyMaps, inputSystemCombinedMap, inputSystemHanziInfo, inputSystemHanziInfoList, inputSystemTemp}
+import inpuSystemLookup.Boundary.runInputSystemLookup
 import serialization.InputSystemSerialization.{readInputSystemFromFileWithBinary, readInputSystemFromFileWithJava, serializeInputSystems}
 import services.inputMethodService.{generateCodeListFromInput, getSortedInfoListsFromCodes, printableCodeListResults, runConsoleProgram}
 import testPreparation.hashmapTestPrepare.{listOf5000Simplified, listOf5000Traditional}
@@ -18,56 +19,8 @@ import com.twitter.util.{Await, Future}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("hej lykke")
-
-    //serializeCedictAndFrequencyFiles()
-    //serializeInputSystems()
-    val zhengma = readInputSystemFromFileWithJava("zhengmaSerialized.txt")
-    //val printing: String = printableCodeListResults(List("zz","ab", "aa", "aavv", "psli", "klg", "boji"), zhengma)
-    //println(printing)
-
-    //*********** run console program
-    runConsoleProgram(zhengma)
-
-    //************* run api data:
-    //val result = apiMethod("sd", zhengma)
-
-
-    println("farvel lykke ")
+    runInputSystemLookup()
   }
 
-  def apiMethod(input: String, inputSystem: inputSystemCombinedMap): String ={
-    val smallLetters: String = input.toLowerCase()
-    val codeList: List[String] = generateCodeListFromInput(smallLetters)
-    val hanziInfoList: List[inputSystemHanziInfo] = getSortedInfoListsFromCodes(codeList, inputSystem)
-    val outpuObject: inputSystemHanziInfoList = inputSystemHanziInfoList(hanziInfoList)
-    // create a JSON string from the Person, then print it
-    implicit val formats = DefaultFormats
-    val jsonString = write(outpuObject)
-    return jsonString
-  }
-
-  def qualityCheckInput(input: codeToTextList): (Integer, Integer) = {
-    val systemCharacterStrings: Predef.Set[String] = input.content.map(i => i.hanzi).toSet
-    val jundacharacters = listOf5000Simplified()//scala.io.Source.fromFile("src/main/resources/frequencyfilesRaw/testJunda.txt").mkString.split("")
-    val tzaicharacters = listOf5000Traditional()
-
-    var missingJunda = new ListBuffer[String]()
-    for (character: String <- jundacharacters) {
-      val existInSet: Boolean = systemCharacterStrings.contains(character)
-      if (!existInSet){
-        missingJunda.addOne(character)
-      }
-    }
-
-    var missingTzai = new ListBuffer[String]()
-    for (character: String <- tzaicharacters) {
-      val existInSet: Boolean = systemCharacterStrings.contains(character)
-      if (!existInSet){
-        missingTzai.addOne(character)
-      }
-    }
-
-    return (missingJunda.length, missingTzai.length)
-  }
 }
+
---
+++ src/main/scala/inpuSystemLookup/Boundary.scala
@@ -0,0 +1,67 @@
+package inpuSystemLookup
+
+import dataClasses.{codeToTextList, inputSystemCombinedMap, inputSystemHanziInfo, inputSystemHanziInfoList}
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json.Serialization.write
+import serialization.InputSystemSerialization.readInputSystemFromFileWithJava
+import services.inputMethodService.{generateCodeListFromInput, getSortedInfoListsFromCodes, runConsoleProgram}
+import testPreparation.hashmapTestPrepare.{listOf5000Simplified, listOf5000Traditional}
+
+import scala.collection.mutable.ListBuffer
+
+object Boundary {
+
+  def runInputSystemLookup(): Unit = {
+    println("hej lykke")
+
+    //serializeCedictAndFrequencyFiles()
+    //serializeInputSystems()
+    val zhengma = readInputSystemFromFileWithJava("zhengmaSerialized.txt")
+    //val printing: String = printableCodeListResults(List("zz","ab", "aa", "aavv", "psli", "klg", "boji"), zhengma)
+    //println(printing)
+
+    //*********** run console program
+    runConsoleProgram(zhengma)
+
+    //************* run api data:
+    //val result = apiMethod("sd", zhengma)
+
+
+    println("farvel lykke ")
+  }
+
+  def apiMethod(input: String, inputSystem: inputSystemCombinedMap): String ={
+    val smallLetters: String = input.toLowerCase()
+    val codeList: List[String] = generateCodeListFromInput(smallLetters)
+    val hanziInfoList: List[inputSystemHanziInfo] = getSortedInfoListsFromCodes(codeList, inputSystem)
+    val outpuObject: inputSystemHanziInfoList = inputSystemHanziInfoList(hanziInfoList)
+    // create a JSON string from the Person, then print it
+    implicit val formats = DefaultFormats
+    val jsonString = write(outpuObject)
+    return jsonString
+  }
+
+  def qualityCheckInput(input: codeToTextList): (Integer, Integer) = {
+    val systemCharacterStrings: Predef.Set[String] = input.content.map(i => i.hanzi).toSet
+    val jundacharacters = listOf5000Simplified()//scala.io.Source.fromFile("src/main/resources/frequencyfilesRaw/testJunda.txt").mkString.split("")
+    val tzaicharacters = listOf5000Traditional()
+
+    var missingJunda = new ListBuffer[String]()
+    for (character: String <- jundacharacters) {
+      val existInSet: Boolean = systemCharacterStrings.contains(character)
+      if (!existInSet){
+        missingJunda.addOne(character)
+      }
+    }
+
+    var missingTzai = new ListBuffer[String]()
+    for (character: String <- tzaicharacters) {
+      val existInSet: Boolean = systemCharacterStrings.contains(character)
+      if (!existInSet){
+        missingTzai.addOne(character)
+      }
+    }
+
+    return (missingJunda.length, missingTzai.length)
+  }
+}
