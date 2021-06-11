4a21ef129dc26c04ef2172280f546226e73c3a87
Weiqifan1 <cmlykke@hotmail.com>
2021-06-11 20:43:07 +0200

    bad imports removed

--- src/main/scala/head/Main.scala
+++ src/main/scala/head/Main.scala
@@ -1,20 +1,6 @@
 package head
 
-import dataClasses.{cedictMaps, codeToTextList, codeToTextObject, frequencyMaps, inputSystemCombinedMap, inputSystemHanziInfo, inputSystemHanziInfoList, inputSystemTemp}
 import inpuSystemLookup.Boundary.runInputSystemLookup
-import serialization.InputSystemSerialization.{readInputSystemFromFileWithBinary, readInputSystemFromFileWithJava, serializeInputSystems}
-import services.inputMethodService.{generateCodeListFromInput, getSortedInfoListsFromCodes, printableCodeListResults, runConsoleProgram}
-import testPreparation.hashmapTestPrepare.{listOf5000Simplified, listOf5000Traditional}
-
-//json
-import scala.collection.mutable._
-import net.liftweb.json._
-import net.liftweb.json.Serialization.write
-
-import scala.collection.mutable.ListBuffer
-import com.twitter.finagle.{Http, Service}
-import com.twitter.finagle.http
-import com.twitter.util.{Await, Future}
 
 object Main {
 
